9749ef69199bc32dd7e85aa53498f63c77257006
choyoonseo-1 <s_eo4111@naver.com>
2025-06-30 13:00:43 +0900

    Add files via upload

---
+++ ch01/IveMember.java
@@ -0,0 +1,14 @@
+package ch01;
+// 클래스 변수
+ class IveMember {
+    // 장원영을 표현할 수 있는
+    // Properties(멤버) - 속성
+    String name;
+    int age;
+    double height;
+
+    // 메서드(Method)
+     void dance() {
+         System.out.println("장원영이 춤춰요");
+     }
+}
---
+++ ch01/TernaryOpr.java
@@ -0,0 +1,22 @@
+package ch01;
+
+// 3항 연산자(?)
+public class TernaryOpr {
+    public static void main(String[] args) {
+        int n1 = 5;
+        // 조건비교? 참일때:거짓일때
+        // n1과 10을 비교?
+        // 10보다 큰 값인지, 작은 값인지 출력
+        String result;    // 두 수를 비교한 결과
+
+        result = n1 > 10 ? " 크다 " : " 작다 ";
+        System.out.println(result);
+
+        // score 변수 생성 : 시험점수 70
+        // 점수가 60이상이면 Pass, Fail을 출력하도록 하세요.
+        int score = 70;
+        String x = score >= 60 ? "Pass" : "Fail";
+        System.out.println(x);
+
+    }
+}
